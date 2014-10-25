6e8822fe0f8e9be5a95ae52c5aa4170ef23501b8
Kutsenko Andrey <andr.donetsk@gmail.com>
2014-10-25 19:39:44 +0300

    upfate graph

--- public/js/controllers/graph.js
+++ public/js/controllers/graph.js
@@ -8,25 +8,30 @@ angular.module('watson')
 				serverService.readDataForGraph()
 					.then(function(data) {
 						$('#graph-container').highcharts({
-        			chart: {
-            		type: 'line'
-        			},
-        			title: {
-            		text: 'Temperature in Sanya flat'
-        			},
-        			xAxis: {
-            		categories: ['Time']
-        			},
-        			yAxis: {
-            		title: {
-                	text: 'Temperature'
-            		}
-        			},
-        			series: [{
-            		name: 'sensor 20',
-            		data: data.data
-        				}]
-    				});
+                         chart: {
+                          type: 'line'
+                      },
+                      title: {
+                          text: 'Temperature in Sanya flat'
+                      },
+                      xAxis: {
+                        categories: ['Time'],
+                        tickInterval: 12,
+                        type: 'datetime'
+                      },
+                      yAxis: {
+                          title: {
+                           text: 'Temperature'
+                       }
+                   },
+                   series: [{
+                      name: 'sensor 20',
+                      animation: false,
+                      data: data.data
+
+                      //pointInterval: 24 * 3600
+                  }]
+              });
     				console.log(data);
 					})
 				}, 5000);
