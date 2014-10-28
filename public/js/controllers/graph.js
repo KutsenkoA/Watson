9f88c7733b087d07f35a8523d8cf928400bcd778
Kutsenko Andrey <andr.donetsk@gmail.com>
2014-10-28 07:49:45 +0200

    tuning graph

--- public/js/controllers/graph.js
+++ public/js/controllers/graph.js
@@ -6,32 +6,34 @@ angular.module('watson')
 
 			$interval(function() {
 				serverService.readDataForGraph()
-					.then(function(data) {
-						$('#graph-container').highcharts({
-                         chart: {
-                          type: 'line'
-                      },
-                      title: {
-                          text: 'Temperature in Sanya flat'
-                      },
-                      xAxis: {
-                        categories: ['Time'],
-                        tickInterval: 12,
-                        type: 'datetime'
-                      },
-                      yAxis: {
-                          title: {
-                           text: 'Temperature'
-                       }
-                   },
-                   series: [{
-                      name: 'sensor 20',
-                      animation: false,
-                      data: data.data
-
-                      //pointInterval: 24 * 3600
-                  }]
-              });
-					})
-				}, 5000);
+          .then(function(data) {
+            $('#graph-container').highcharts({
+              chart: {
+                type: 'line'
+              },
+              title: {
+                text: 'Temperature in Sanya flat'
+              },
+              xAxis: {
+                tickInterval: 3600 * 1000,
+                type: 'datetime'
+              },
+              yAxis: {
+                title: {
+                  text: 'Temperature'
+                }
+              },
+              series: [{
+                name: 'sensor 20',
+                animation: false,
+                data: data.data
+              }],
+              plotOptions: {
+                series: {
+                  pointInterval: 3600 * 1000
+                }
+              }
+            });
+          })
+      }, 5000);
 		}]);
